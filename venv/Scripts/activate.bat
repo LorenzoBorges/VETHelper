bdcc7eddcfcd6d464a2ed97dadd56866787fbd54
Lorenzo <lorenzocsborges@gmail.com>
2020-10-19 01:28:03 -0300

    ajustes no venv, considerando que formatei o PC

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Projetos\projeto-veterinario\venv
+set VIRTUAL_ENV=D:\Documentos\Estudos\Programação\Arquivos Python\Programas\VETHelper\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
